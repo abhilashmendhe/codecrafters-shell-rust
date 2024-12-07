158bbb8e94bb42699a17ea770a5a5830d932c6fd
Abhi <abhilashmendhe@gmail.com>
2024-12-07 12:17:33 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,26 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         
-        let trim_inp = input.trim();
-        if trim_inp == "exit 0" {
+        let trim_input = input.trim();
+        if trim_input == "exit 0" {
             break;
         }
 
-        if trim_inp.starts_with("echo") {
-            println!("{}",&trim_inp[5..]);
+        if trim_input.starts_with("echo") {
+            println!("{}",&trim_input[5..]);
+        } else if trim_input.starts_with("type") {
+            let builtin = ["echo", "exit", "type"];
+            let mut t_input_spl = trim_input.split(" ");
+            let _ = t_input_spl.next().unwrap();
+            let command_str = t_input_spl.next().unwrap();
+
+            if builtin.contains(&command_str) {
+                println!("{} is a shell builtin",command_str);
+            } else {
+                println!("{} not found",command_str);
+            }
         } else {
-            println!("{}: command not found", trim_inp);
+            println!("{}: command not found", trim_input);
         }
     }
 }
