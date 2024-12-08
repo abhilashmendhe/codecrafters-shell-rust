a103d759a7f3aca276a62be951084b472f6e297b
Abhi <abhilashmendhe@gmail.com>
2024-12-08 14:23:32 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 use codecrafters_shell::search_directory;
 
@@ -25,11 +25,34 @@ fn main() {
 
         if trim_input.starts_with("echo") {
             println!("{}",&trim_input[5..]);
+
+        } else if trim_input.starts_with("cd") {
+            
+            let mut t_input_spl = trim_input.split(" ");
+            let _ = t_input_spl.next().unwrap();
+
+            if let Some(path) = t_input_spl.next() {
+
+                let s_path = Path::new(path);
+                let res = std::env::set_current_dir(&s_path);
+                if res.is_err() {
+                    println!("cd: {}: No such file or directory", path);
+                }
+    
+            } else {
+
+                let home_var = std::env::var("HOME").unwrap();
+                let s_path = Path::new(&home_var);
+                let _ = std::env::set_current_dir(&s_path);
+
+            }
+
         } else if trim_input.starts_with("pwd") {
             let current = std::env::current_dir().unwrap();
             println!("{}",current.to_str().unwrap());
+
         } else if trim_input.starts_with("type") {
-            let builtin = ["echo", "exit", "type", "pwd"];
+            let builtin = ["echo", "exit", "type", "pwd", "cd"];
             let mut t_input_spl = trim_input.split(" ");
             let _ = t_input_spl.next().unwrap();
             let command_str = t_input_spl.next().unwrap();
